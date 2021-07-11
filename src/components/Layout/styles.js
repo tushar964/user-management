749a976a28898bd6d5cc29c3b4c2af99993c6b11
tushar kale <tusharkale200@gmail.com>
2021-07-11 18:07:45 +0530

    added styled components and layout

---
+++ src/components/Layout/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  background: #ccc;
+  margin: auto;
+  height: 100vh;
+  padding: 30px;
+`;
+export const PageContainer = styled.div`
+  background: #5dade2;
+  min-height: 500px;
+  padding: 30px;
+  border-radius: 10px;
+`;
