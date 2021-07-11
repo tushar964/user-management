9a75b2f9c18be5e682165aded99f6c9ef7591408
tushar kale <tusharkale200@gmail.com>
2021-07-11 19:02:25 +0530

    added class component for dashboard

--- src/containers/dashboard/index.js
+++ src/containers/dashboard/index.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Dashboard = () => {
-  return <div>dashboard</div>;
-};
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageName: "Dashboard",
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.pageName}
+        <div>user table will be displayed here</div>
+      </div>
+    );
+  }
+}
+
 export default Dashboard;
